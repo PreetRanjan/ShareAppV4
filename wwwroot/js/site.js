211528276ca2fd4eaa38030d6394dbe93563aca9
Preet Ranjan <preetish.888@gmail.com>
2023-11-01 10:24:25 +0530

    first commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+//(function () {
+//    'use strict'
+
+//    document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
+//        document.querySelector('.offcanvas-collapse').classList.toggle('open')
+//    })
+//})()
